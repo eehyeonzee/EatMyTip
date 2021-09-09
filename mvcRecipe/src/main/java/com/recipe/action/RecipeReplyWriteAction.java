691a0a54a7eca054a1e840bc994f584d8341a4cf
dhdld <dhdld@192.168.123.186>
2021-09-09 16:52:58 +0900

    레시피 댓글 VO작성

--- mvcRecipe/src/main/java/com/recipe/action/RecipeReplyWriteAction.java
+++ mvcRecipe/src/main/java/com/recipe/action/RecipeReplyWriteAction.java
@@ -1,7 +1,11 @@
 package com.recipe.action;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.controller.Action;
 
@@ -10,6 +14,15 @@ public class RecipeReplyWriteAction implements Action{
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
+		Map<String, String> mapAjax = new HashMap<String, String>();
+		
+		HttpSession session = request.getSession();
+		Integer mem_num = (Integer)session.getAttribute("mem_num");
+		if(mem_num == null) {	// 로그인이 안된 경우
+			mapAjax.put("result", "logout");
+		}else {	// 로그인 된 경우
+		
+		}
 		return "/WEB-INF/views/common/ajax_view.jsp";
 	}
 
---
+++ mvcRecipe/src/main/java/com/recipe/vo/RecipeCommendsVO.java
@@ -0,0 +1,76 @@
+package com.recipe.vo;
+
+public class RecipeCommendsVO {
+	private int comm_num;
+	private int board_num;
+	private int mem_num;
+	private String comm_date;
+	private String comm_con;
+	private int qna_num;
+	private int news_num;
+	private String comm_modifydate;
+	private String id;
+	private int auth;
+	public int getComm_num() {
+		return comm_num;
+	}
+	public void setComm_num(int comm_num) {
+		this.comm_num = comm_num;
+	}
+	public int getBoard_num() {
+		return board_num;
+	}
+	public void setBoard_num(int board_num) {
+		this.board_num = board_num;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public String getComm_date() {
+		return comm_date;
+	}
+	public void setComm_date(String comm_date) {
+		this.comm_date = comm_date;
+	}
+	public String getComm_con() {
+		return comm_con;
+	}
+	public void setComm_con(String comm_con) {
+		this.comm_con = comm_con;
+	}
+	public int getQna_num() {
+		return qna_num;
+	}
+	public void setQna_num(int qna_num) {
+		this.qna_num = qna_num;
+	}
+	public int getNews_num() {
+		return news_num;
+	}
+	public void setNews_num(int news_num) {
+		this.news_num = news_num;
+	}
+	public String getComm_modifydate() {
+		return comm_modifydate;
+	}
+	public void setComm_modifydate(String comm_modifydate) {
+		this.comm_modifydate = comm_modifydate;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getAuth() {
+		return auth;
+	}
+	public void setAuth(int auth) {
+		this.auth = auth;
+	}
+	
+	
+}
